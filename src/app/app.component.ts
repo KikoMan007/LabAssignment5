6375a2275e7f794a92649cac178acd2f39fdace5
MichaelAsavamedhi <asavamedhi@yahoo.com>
2019-05-02 01:09:56 -0700

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'LabAssignment3';
+  
+
+
+
+
+  constructor() {
+  }
+
+  showGreeting() {
+    const row2 = document.getElementById('r1c2');
+    this.greeting = 'Hello' + this.firstName + '' + this.lastName + '.'
+
+  }
+  resetGreeting() {
+    const row2 = document.getElementById('r1c2');
+    this.greeting = 'r1c2';
+
+  }
 }
