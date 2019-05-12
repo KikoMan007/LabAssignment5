ecdacf056f4e618830498847cb726d60cfa72d51
MichaelAsavamedhi <asavamedhi@yahoo.com>
2019-05-11 17:07:16 -0700

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
 import { MainContentComponent } from './main-content/main-content.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialDesignModule } from '../material-design/material-design.module';
+
 
 @NgModule({
   declarations: [
@@ -15,8 +18,9 @@ import { MainContentComponent } from './main-content/main-content.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
-
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialDesignModule
   ],
   providers: [],
   bootstrap: [AppComponent]
